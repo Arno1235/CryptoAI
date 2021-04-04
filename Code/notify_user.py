a7e782242a1a5ddb4926d04e1bb274554ce22524
Arno Van Eetvelde <arno.ve2000@gmail.com>
2021-04-04 11:25:49 +0200

    updating notify_user

---
+++ Code/notify_user.py
@@ -0,0 +1,7 @@
+# Notify user via Discord
+# Log everything in an excel file (maybe calculate the predicted profit vs the actual profit)
+
+if __name__ == "__main__":
+    print("running notify_user")
+
+    
