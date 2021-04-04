211ad55b0c443243cfd9de511a958037322a5c30
Arno Van Eetvelde <arno.ve2000@gmail.com>
2021-04-04 17:46:09 +0200

    Update notify_user.py

--- Code/notify_user.py
+++ Code/notify_user.py
@@ -1,7 +1,17 @@
-# Notify user via Discord
-# Log everything in an excel file (maybe calculate the predicted profit vs the actual profit)
+# TODO: Log everything in an excel file (maybe calculate the predicted profit vs the actual profit)
+
+from discord import Webhook, RequestsWebhookAdapter
+
+def notifyUser(message):
+    webhook = Webhook.from_url(getDiscordURL(), adapter=RequestsWebhookAdapter())
+    webhook.send(message)
+
+def getDiscordURL():
+    with open ("secret.txt", "r") as myfile:
+        data = myfile.read().splitlines()
+        return data[2]
 
 if __name__ == "__main__":
     print("running notify_user")
 
-    
+    notifyUser("Test")
