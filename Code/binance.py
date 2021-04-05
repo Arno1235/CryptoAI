712aee1db8cb5e48593c620d456126a45232e099
Arno Van Eetvelde <arno.ve2000@gmail.com>
2021-04-05 12:46:02 +0200

    Update binance.py

--- Code/binance.py
+++ Code/binance.py
@@ -1,6 +1,30 @@
 from binance_mod.client import Client
+from datetime import datetime, timedelta
 
+class Binance:
+
+    def __init__(self, fmt, symbol):
+        self.loadAPISecret()
+        self.client = Client(api_key=self.api_key, api_secret=self.api_secret)
+        self.fmt = fmt
+        self.symbol = symbol
+    
+    def loadAPISecret(self):
+        with open ("secret.txt", "r") as myfile:
+            data = myfile.read().splitlines()
+            self.api_key = data[0]
+            self.api_secret = data[1]
+    
+    def getData(self, hours, time_interval=Client.KLINE_INTERVAL_1MINUTE):
+        hours = str(hours) + " hour ago UTC"
+        klines = self.client.get_historical_klines(self.symbol, time_interval, hours)
+        if len(klines) < 1:
+            print("Failed to download data from %s for %s." % (self.symbol, hours))
 
 
 if __name__ == "__main__":
     print("running binance")
+
+    binance = Binance("", "BTCEUR")
+
+    binance.getData(1)
