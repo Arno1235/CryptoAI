3c4b7bb5a1d51a9195b965a2d8f863059e7e595e
Arno Van Eetvelde <arno.ve2000@gmail.com>
2021-04-05 15:06:44 +0200

    Update binance.py

--- Code/binance.py
+++ Code/binance.py
@@ -3,10 +3,9 @@ import pandas as pd
 
 class Binance:
 
-    def __init__(self, fmt, symbol):
+    def __init__(self, symbol):
         self.loadAPISecret()
         self.client = Client(api_key=self.api_key, api_secret=self.api_secret)
-        self.fmt = fmt
         self.symbol = symbol
 
         self.org_columns = ['timestamp', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'quote_av', 'trades', 'tb_base_av', 'tb_quote_av', 'ignore']
@@ -18,25 +17,25 @@ class Binance:
             self.api_key = data[0]
             self.api_secret = data[1]
     
-    def getData(self, hours, time_interval=Client.KLINE_INTERVAL_1MINUTE):
-        hours = str(hours) + " hour ago UTC"
-        klines = self.client.get_historical_klines(self.symbol, time_interval, hours)
+    def getData(self, hours_ago, time_interval=Client.KLINE_INTERVAL_1MINUTE):
+        hours_ago = str(hours_ago) + " hour ago UTC"
+        klines = self.client.get_historical_klines(self.symbol, time_interval, hours_ago)
         if len(klines) < 1:
-            print("Failed to download data from %s for %s." % (self.symbol, hours))
+            print("Failed to download data from %s for %s." % (self.symbol, hours_ago))
             return
-        print("Succesfully downloaded %s lines of data from %s for %s" % (str(len(klines)), self.symbol, hours))
+        print("Succesfully downloaded %s lines of data from %s for %s" % (str(len(klines)), self.symbol, hours_ago))
 
         self.df = pd.DataFrame(klines, columns=self.org_columns)
         self.df['timestamp'] = pd.to_datetime(self.df['timestamp'], unit='ms')
         self.df.set_index('timestamp', inplace=True)
         self.df.drop(columns=[item for item in self.org_columns if item not in self.imp_columns], axis=1, inplace=True)
-        
-        print(self.df)
 
 
 if __name__ == "__main__":
     print("running binance")
 
-    binance = Binance("", "BTCEUR")
+    binance = Binance(symbol="BTCEUR")
 
     binance.getData(1)
+
+    print(binance.df)
