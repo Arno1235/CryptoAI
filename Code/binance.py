f532ada6c9139e701c0c40682ca52d8c0bb2ff4e
Arno Van Eetvelde <arno.ve2000@gmail.com>
2021-04-05 15:27:25 +0200

    Update binance.py

--- Code/binance.py
+++ Code/binance.py
@@ -17,6 +17,25 @@ class Binance:
             self.api_key = data[0]
             self.api_secret = data[1]
     
+    def getAccountInfo(self):
+        self.accountInfo = self.client.get_account()
+        self.accountStatus = self.client.get_account_status()
+    
+    def getStatus(self):
+        self.status = self.client.get_system_status()["msg"]
+    
+    def getTradeFee(self):
+        self.tradeFee = self.client.get_trade_fee(symbol=self.symbol)
+    
+    def getAllOrders(self):
+        self.allOrders = self.client.get_all_orders(symbol=self.symbol)
+    
+    def getOpenOrders(self):
+        self.openOrders = self.client.get_open_orders(symbol=self.symbol)
+    
+    def getTrades(self):
+        self.trades = self.client.get_my_trades(symbol=self.symbol)
+    
     def getData(self, hours_ago, time_interval=Client.KLINE_INTERVAL_1MINUTE):
         hours_ago = str(hours_ago) + " hour ago UTC"
         klines = self.client.get_historical_klines(self.symbol, time_interval, hours_ago)
@@ -29,6 +48,8 @@ class Binance:
         self.df['timestamp'] = pd.to_datetime(self.df['timestamp'], unit='ms')
         self.df.set_index('timestamp', inplace=True)
         self.df.drop(columns=[item for item in self.org_columns if item not in self.imp_columns], axis=1, inplace=True)
+    
+    
 
 
 if __name__ == "__main__":
@@ -37,5 +58,23 @@ if __name__ == "__main__":
     binance = Binance(symbol="BTCEUR")
 
     binance.getData(1)
-
     print(binance.df)
+
+    binance.getAccountInfo()
+    print(binance.accountInfo)
+    print(binance.accountStatus)
+
+    binance.getStatus()
+    print(binance.status)
+
+    binance.getTradeFee()
+    print(binance.tradeFee)
+
+    binance.getAllOrders()
+    print(binance.allOrders)
+
+    binance.getOpenOrders()
+    print(binance.openOrders)
+
+    binance.getTrades()
+    print(binance.trades)
