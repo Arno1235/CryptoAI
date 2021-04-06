5dd7aab04ccd643bd37aea72b41f73bba4ea3c6a
Arno Van Eetvelde <arno.ve2000@gmail.com>
2021-04-06 02:03:51 +0200

    Update main.py

--- Code/main.py
+++ Code/main.py
@@ -1,7 +1,6 @@
 from binance import *
-from data_manipulation_functions import *
 from neural_network import *
-from extra_analysis import *
+from extra_functions import *
 
 import numpy as np
 
@@ -21,14 +20,17 @@ def predictSymbol(symbol, showPlots=False):
     n_nodes = 30
 
     neuralNetwork = NeuralNetwork(n_layers, n_nodes, n_per_learn, n_per_predict, binance.n_features)
-
     result = neuralNetwork.trainNN(input_X, input_Y)
-
     if showPlots: visualize_training_results(result)
 
     prediction = neuralNetwork.predictionNN(np.array(binance.df.tail(n_per_learn)).reshape(1, n_per_learn, binance.n_features))
     prediction = scalePrediction(prediction, binance.df, binance.close_scaler)
     if showPlots: plotPrediction(prediction)
+
+    # TODO add last datapoint of orignal data to prediciton
+    prediction = convertToPercentages(prediction)
+    print(prediction)
+    plotPrediction(prediction)
 
 if __name__ == "__main__":
     print("running main")
