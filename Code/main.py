c2c2097ae03347e74a9db98d324cee34627394a0
Arno Van Eetvelde <arno.ve2000@gmail.com>
2021-06-17 00:14:05 +0200

    Update main.py

--- Code/main.py
+++ Code/main.py
@@ -4,40 +4,41 @@ from extra_functions import *
 
 import numpy as np
 
-def predictSymbol(symbol, showPlots=False):
+def predictSymbol(symbol, binance, showPlots=False):
 
-    binance = Binance(symbol)
+    coin = Coin(symbol=symbol, binance=binance)
 
-    binance.getData(6)
-    binance.scaleData()
+    coin.getScaledData(hours_ago=1)
 
     n_per_learn = 90
     n_per_predict = 30
 
-    input_X, input_Y = split_sequence(binance.df.to_numpy(), n_per_learn, n_per_predict)
+    input_X, input_Y = split_sequence(coin.df.to_numpy(), n_per_learn, n_per_predict)
 
     n_layers = 1
     n_nodes = 30
 
-    neuralNetwork = NeuralNetwork(n_layers, n_nodes, n_per_learn, n_per_predict, binance.n_features)
+    neuralNetwork = NeuralNetwork(n_layers, n_nodes, n_per_learn, n_per_predict, coin.n_features)
     result = neuralNetwork.trainNN(input_X, input_Y)
     if showPlots: visualize_training_results(result)
 
-    prediction = neuralNetwork.predictionNN(np.array(binance.df.tail(n_per_learn)).reshape(1, n_per_learn, binance.n_features))
-    prediction = scalePrediction(prediction, binance.df, binance.close_scaler)
+    prediction = neuralNetwork.predictionNN(np.array(coin.df.tail(n_per_learn)).reshape(1, n_per_learn, coin.n_features))
+    prediction = scalePrediction(prediction, coin.df, coin.close_scaler)
     if showPlots: plotPrediction(prediction)
 
     # TODO add last datapoint of orignal data to prediciton
     prediction = convertToPercentages(prediction)
     print(prediction)
-    plotPrediction(prediction)
+    if showPlots: plotPrediction(prediction)
 
 if __name__ == "__main__":
     print("running main")
 
     symbol_list = ['BTCUSDT']
 
+    binance = Binance()
+
     while True:
         for symbol in symbol_list:
-            predictSymbol(symbol, showPlots=True)
+            predictSymbol(symbol, binance, showPlots=True)
         break
