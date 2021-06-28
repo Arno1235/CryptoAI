d2f75e8f3bad9485a967681aa616a7778c45369e
Arno Van Eetvelde <arno.ve2000@gmail.com>
2021-06-28 18:19:56 +0200

    Update main.py

--- Code/main.py
+++ Code/main.py
@@ -1,9 +1,14 @@
 from binance import Binance, Coin
-from neural_network import NeuralNetwork
+from neural_network import NeuralNetwork, ACCURACY_THRESHOLD
 from extra_functions import *
 from notify_user import notifyUser, Log
+from interpret_data import minmax_interpration
 
+import datetime
+import time
 import numpy as np
+
+
 
 def predictSymbol(symbol, binance, showPlots=False, log=Log()):
 
@@ -28,20 +33,34 @@ def predictSymbol(symbol, binance, showPlots=False, log=Log()):
     # print(prediction)
     if showPlots: plotPrediction(prediction)
 
-    prediction = convertToPercentages(prediction)
-    if showPlots: plotPrediction(prediction)
+    first_value = prediction['close'].iloc[0]
+
+    prediction_in_percentages = convertToPercentages(prediction)
+    if showPlots: plotPrediction(prediction_in_percentages)
+
+    acc = result.history['accuracy'][-1]
+    
+    if acc > ACCURACY_THRESHOLD:
+        interpretation = minmax_interpration(prediction=prediction_in_percentages, first_value=first_value)
+        if interpretation != False:
+            min_sell, max_sell = interpretation
+            notifyUser("You should buy %s and sell at %f and %f, I am %f sure." %(symbol, min_sell, max_sell, acc))
+            log.write("TEST: buy %s and sell at %f and %f, accuracy = %f" %(symbol, min_sell, max_sell, acc))
 
     log.save()
 
 if __name__ == "__main__":
     print("running main")
 
-    symbol_list = ['BTCEUR']
+    symbol_list = ['BTCEUR', 'DOGEEUR', 'LTCEUR', 'VETEUR', 'ADAEUR', 'ETHEUR']
 
     binance = Binance()
     log = Log()
 
     while True:
         for symbol in symbol_list:
-            predictSymbol(symbol, binance, showPlots=True, log=log)
-        break
+            time0 = datetime.datetime.now()
+            predictSymbol(symbol=symbol, binance=binance, showPlots=False, log=log)
+            timediff = (datetime.datetime.now() - time0).total_seconds()
+            if timediff > 0:
+                time.sleep(timediff)
